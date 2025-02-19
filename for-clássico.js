fe9e22512ea7f226a719267cae6b6d9c8297986a
Matheus Goncalves <matheus.goncalves@parceiro.niw.pt>
2025-02-19 20:55:02 +0000

    novas instruções

---
+++ for-clássico.js
@@ -0,0 +1,10 @@
+const notas = [10, 8.5, 5, 6.5, 8, 7.5];
+
+
+// primeira expressão: executada apenas uma vez(iniciação de variável)
+// segunda expressão: condição de execução
+// terceira expressão: é executada oa final do bloco
+
+for (let indice = 0; indice < notas.length; indice++) {
+    console.log(indice, notas[indice]);
+}
